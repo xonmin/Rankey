23980667dd396c74b9bb73c1507f88954808c1fb
han51361 <han51361@gmail.com>
2022-09-05 22:06:09 +0900

    init commit

---
+++ src/main/kotlin/com/xonmin/rankey/RankeyApplication.kt
@@ -0,0 +1,11 @@
+package com.xonmin.rankey
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RankeyApplication
+
+fun main(args: Array<String>) {
+    runApplication<RankeyApplication>(*args)
+}
---
+++ src/test/kotlin/com/xonmin/rankey/RankeyApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.xonmin.rankey
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RankeyApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
