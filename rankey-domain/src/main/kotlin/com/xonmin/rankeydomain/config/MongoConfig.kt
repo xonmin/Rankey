68b27875e0d882b067047cf2c36293a859a6ff6d
han51361 <han51361@gmail.com>
2022-09-24 21:40:06 +0900

    add : mongoConfig

---
+++ rankey-domain/src/main/kotlin/com/xonmin/rankeydomain/config/MongoConfig.kt
@@ -0,0 +1,36 @@
+package com.xonmin.rankeydomain.config
+
+import org.springframework.boot.autoconfigure.mongo.MongoProperties
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.mongodb.MongoDatabaseFactory
+import org.springframework.data.mongodb.MongoTransactionManager
+import org.springframework.data.mongodb.config.EnableMongoAuditing
+import org.springframework.data.mongodb.core.MongoTemplate
+import org.springframework.data.mongodb.core.SimpleMongoClientDatabaseFactory
+
+@Configuration
+@EnableMongoAuditing
+class MongoConfig {
+    @Bean
+    @ConfigurationProperties("spring.data.mongodb")
+    fun properties(): MongoProperties {
+        return MongoProperties()
+    }
+
+    @Bean
+    fun mongoFactory(properties: MongoProperties): MongoDatabaseFactory {
+        return SimpleMongoClientDatabaseFactory(properties.uri)
+    }
+
+    @Bean
+    fun mongoTemplate(mongoFactory: MongoDatabaseFactory): MongoTemplate {
+        return MongoTemplate(mongoFactory)
+    }
+
+    @Bean
+    fun transactionManager(mongoFactory: MongoDatabaseFactory): MongoTransactionManager {
+        return MongoTransactionManager(mongoFactory)
+    }
+}
